95bb039130c84fbeef7458aa3eb198214776dfb6
CHOSSENNG <jng00153@gmail.com>
2024-08-24 15:04:01 +0900

    🎉 토비 스프링 예제 시작

---
+++ src/main/java/springbook/TobiSpringSudyApplication.java
@@ -0,0 +1,13 @@
+package springbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiSpringSudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobiSpringSudyApplication.class, args);
+    }
+
+}
